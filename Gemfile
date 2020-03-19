f1470e0e263088762536bfa9f5860b7f75e71621
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-19 14:24:35 +0000

    added details to get_teams method

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in nba.gemspec
 gemspec
+
+gem 'httparty', '~> 0.13.7'
--- lib/nba/api.rb
+++ lib/nba/api.rb
@@ -2,7 +2,9 @@ class API
   def self.get_teams
       response = HTTParty.get('http://data.nba.net/10s/prod/v1/2016/players.json')
 
-      binding.pry
+
+      @@all_players = response['league']['standard']
+      #binding.pry
 
   end
 end
