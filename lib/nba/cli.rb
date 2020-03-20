0a043f325df5bb0d4b59f9f2ce4a49474204a3dd
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-20 17:36:36 +0000

    changed everything

--- lib/nba/cli.rb
+++ lib/nba/cli.rb
@@ -1,42 +1,32 @@
 #CLI controller
-
 class CLI
-
   def start
     self.welcome
     self.get_players_list
   end
-
   def welcome
     puts 'welcome to the player stats portal'
     #sleep(2)
     puts 'please select a player'
   end
-
   def get_players_list
     # binding.pry
       API.get_players #calls API to get list of NBA teams
-
-        self.parse_players
+        # self.parse_players
       self.display_players_list
   end
-
-  def parse_players
-      @@all = Player.all.each {|player| puts player.firstName}
-#binding.pry
-   end
-
+#   def parse_players
+#       @@all = Player.all.each {|player| puts player.firstName}
+# #binding.pry
+#    end
    def self.all
      @@all
    end
-
   def display_players_list
     Player.all.each.with_index(1) do |player, idx|
-      puts "#{idx}. #{player}"
+      puts "#{idx}. #{player.firstName} #{player.lastName}"
   end
-
-
-
+  # def get user inpute
+  #   player.all
 end
-
 end
