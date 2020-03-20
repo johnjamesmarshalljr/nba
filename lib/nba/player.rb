eaa8e8ad2be7d7bc909cf0d1f8a550dc9eed496d
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-20 17:45:50 +0000

    spacing

--- lib/nba/player.rb
+++ lib/nba/player.rb
@@ -1,22 +1,40 @@
 class Player
+
   @@all = []
+
   attr_accessor :firstName, :lastName, :personId, :teamId, :jersey, :pos, :heightFeet, :heightInches, :heightMeters, :weightPounds, :weightKilograms, :dateOfBirthUTC, :teams, :draft, :nbaDebutYear, :yearsPro, :collegeName, :lastAffiliation, :country
+
     def initialize(attributes)
         attrs_from_hash(attributes)
         @@all << self
+
     end
     # def self.puts_name
     #   self.each do |player| puts self.name end
     # end
+
     def attrs_from_hash(attributes)
+
       attributes.each {|key, value| self.send(("#{key}="), value)}
+
     end
+
     def self.all
+
       @@all
+
     end
-    # def self.get_players
+
+
+
+  # def self.get_players
+
     #   API.get_players
+
     #   all
+
     #
+
     # end
+
 end
